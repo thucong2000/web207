f3575cd37f8afc4b021453f09beaaa205e077b55
yentacute <57887927+yentacute@users.noreply.github.com>
2021-06-29 23:26:58 +0700

    Push code

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const product = {
+  name: 'product 1',
+  price: 20,
+  status: true
+}
+
+ReactDOM.render(
+  product.name,
+  document.getElementById('root')
+);
+
+//tất cả phần tử được render ra dom đều là đối tượng react tạo ra
